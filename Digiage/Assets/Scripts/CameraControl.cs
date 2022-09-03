06c9328f54958f031adc742f12813aeb680f5c46
catcoder03 <b_dd0_huseyin_06@outlook.com>
2022-09-03 22:02:03 +0300

    second
    kamera + hareket

---
+++ Digiage/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);  
+    }
+}
---
+++ Digiage/Assets/Scripts/Walk.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+    public Rigidbody2D rb;
+    public Animator animator;
+    Vector2 movement;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Horizontal = Input.GetAxisRaw("Horizontal");
+        float Vertical = Input.GetAxisRaw("Vertical");
+
+        /*if (Horizontal == 0 && Vertical == 0)
+        {
+            rb.velocity = new Vector2(0, 0);
+            return;
+        }*/
+        movement = new Vector2(Horizontal, Vertical);
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+        //rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rb.velocity = new Vector2(movement.x * moveSpeed, movement.y * moveSpeed);
+    }
+}
